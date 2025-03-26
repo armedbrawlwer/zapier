57a49475b1a4dc985022506e342501003a6be858
armedbrawlwer <tripathi.abhishek2018@gmail.com>
2025-03-26 16:18:55 +0530

    updated schema and server started

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,9 +1,32 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 
 const app = express();
+const client = new PrismaClient()
+app.listen(3000)
+app.use(express.json())
 
-app.post('/hooks/catc/:userid/:zapid',(req,res)=>{
+app.post('/hooks/catch/:userid/:zapid', async (req, res) => {
     const userid = req.params.userid;
-    const zapid = req.params.zapid;
-    
-})
+    const zapId = req.params.zapid;
+    const body = req.body;
+
+    //store in db a new trigger
+    await client.$transaction(async tx => {
+        const run = await client.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        })
+        await client.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
+    })
+    res.json({
+        message:"webhook recieved"
+    })
+}
+)
